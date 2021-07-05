9dde29ab8db4069ebe5d773ab3fb6416b55ed928
Abhishek Shah <abhishekshah3@gmail.com>
2021-07-05 18:39:14 +0530

    working tcp client and server

--- tcp_client/src/main.rs
+++ tcp_client/src/main.rs
@@ -1,28 +1,30 @@
 #![allow(unused)]
-use std::io;
+use std::io::{self, BufRead, BufReader};
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 fn main() {
     let mut package = String::with_capacity(4096);
-    let mut buf = [0u8; 4096];
+    let mut server = TcpStream::connect("127.0.0.1:3000").unwrap();
+    
     loop {
-        let mut server = TcpStream::connect("127.0.0.1:3000").unwrap();
-        println!("Enter input");
-        io::stdin().read_line(&mut package).unwrap();
-        println!("Status of package : {} ", package);
+        println!("Enter input\n>");
+        io::stdin().read_line(&mut package).expect("Failed to read from STDIN");
+        // println!("Sending \"{}\" ...", package);
 
         if package.trim() == "quit".to_string() {
             println!("Exiting ...");
             std::process::exit(0);
         }
-        let bytes_sent = server.write(&mut package.as_bytes()).unwrap();
+        // Send `package` as bytes into stream
+        let bytes_sent = server.write(&mut package.as_bytes()).expect("Failed to write to server");
         package.clear();
 
-        let bytes_recvd = server.read(&mut buf).unwrap();
+        // Create a BufReader and read until `\n` char
+        let mut reader = BufReader::new(&server);
+        reader.read_line(&mut package).expect("Failed to read into buffer");
 
-        assert_eq!(bytes_recvd, bytes_sent);
-
-        println!("Server says : {}", String::from_utf8_lossy(&buf));
+        println!("Server says : {}", package);
+        package.clear();
         println!("{}", "*".repeat(20));
     }
 }
--- tcp_server/src/main.rs
+++ tcp_server/src/main.rs
@@ -1,29 +1,29 @@
 #![allow(unused)]
 use core::str;
-use std::io::{Read, Write};
+use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{TcpListener, TcpStream};
 fn main() {
     let socket = TcpListener::bind("127.0.0.1:3000").unwrap();
-    // for stream in socket.incoming() {
-    //     if let Ok(mut stream) = stream {
-    //         println!(
-    //             "connection established with {}",
-    //             stream.peer_addr().unwrap()
-    //         );
-    //         let mut buf = [0u8; 4096 * 2];
-    //         let bytes_read = stream.read(&mut buf).unwrap();
-    //         stream.write(&mut buf[..bytes_read]).unwrap();
-    //     }
-    // }
+    // let mut buf = [0u8; 4096];
+    let mut buf_string = String::new();
+    for stream in socket.incoming() {
+        if let Ok(mut stream) = stream {
+            println!(
+                "connection established with {}",
+                stream.peer_addr().unwrap()
+            );
+            let mut reader = BufReader::new(&stream);
+            loop {
+                // loop is required to keep working with the stream it is connected to
 
-    let (mut stream, socket) = socket.accept().unwrap();
-    // dbg!(socket);
-    let mut buf = [0u8; 1024*4];
-    loop {
-        let bytes_read = stream.read(&mut buf).unwrap();
-        println!("read {}", String::from_utf8_lossy(&buf));
-        assert!(bytes_read <= 4096);
-        stream.write(&buf[..bytes_read]).unwrap();
+                let bytes_read = reader.read_line(&mut buf_string).unwrap();
+                if bytes_read == 0 {
+                    continue;
+                }
+                let bytes_written = (&stream).write(buf_string.as_bytes()).unwrap();
+                println!("{} bytes written", bytes_written);
+                buf_string.clear();
+            }
+        } // stream dropped here
     }
-
 }
