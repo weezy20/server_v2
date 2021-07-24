e5a9c645ba25a6883c73855acbd56f6a56ea517c
Abhishek Shah <abhishekshah3@gmail.com>
2021-07-24 15:11:51 +0530

    tweaked tcp_client

--- tcp_client/src/main.rs
+++ tcp_client/src/main.rs
@@ -4,11 +4,14 @@ use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 fn main() {
     let mut package = String::with_capacity(4096);
-    let mut server = TcpStream::connect("127.0.0.1:3000").unwrap();
     
     loop {
+        let mut server = TcpStream::connect("127.0.0.1:3000").unwrap();
         println!("Enter input\n>");
-        io::stdin().read_line(&mut package).expect("Failed to read from STDIN");
+        io::stdout().flush().expect("failed to flush");
+        io::stdin()
+            .read_line(&mut package)
+            .expect("Failed to read from STDIN");
         // println!("Sending \"{}\" ...", package);
 
         if package.trim() == "quit".to_string() {
@@ -16,12 +19,17 @@ fn main() {
             std::process::exit(0);
         }
         // Send `package` as bytes into stream
-        let bytes_sent = server.write(&mut package.as_bytes()).expect("Failed to write to server");
+        let bytes_sent = server
+            .write(&mut package.as_bytes())
+            .expect("Failed to write to server");
+
         package.clear();
 
         // Create a BufReader and read until `\n` char
         let mut reader = BufReader::new(&server);
-        reader.read_line(&mut package).expect("Failed to read into buffer");
+        reader
+            .read_line(&mut package) //Reads uptil \n 0xA 
+            .expect("Failed to read into buffer");
 
         println!("Server says : {}", package);
         package.clear();
